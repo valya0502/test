b6d86f57d0dd3fddb2546364205f8abac67f68f6
valya0502 <valya0502@gmail.com>
2018-09-05 20:36:36 +0300

    test
    test

---
+++ public_html/index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>BINO</title>	
+	<!-- <link rel="shortcut icon" href="/img/favicon.png" type="image/png"> -->
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/responsive.css" />
+	<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+	<!-- Add the slick-theme.css if you want default styling -->
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
+	<!-- Add the slick-theme.css if you want default styling -->
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+</head>
+<body>
+	
+	<header class="header">
+		<div class="container">
+			<div class="header_content">
+				<div class="row justify-content-between">
+					<div class="col-lg-2 col-md-2 col-sm-12">
+						<a href="/"><img src="img/Logo.png" class="logo" alt="logo"></a>
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-12   p-0 ">
+						<!-- burger menu -->
+					    <div class="wrapper">
+					        <input type="checkbox" id="check-menu">
+					        <label for="check-menu"></label>
+					        <div class="burger-line first"></div>
+					        <div class="burger-line second"></div>
+					        <div class="burger-line third"></div>
+					        <div class="burger-line fourth"></div>
+					        <nav class="main-menu">
+					            <a href="#" class="burger_menu_active">HOME</a>
+					            <a href="#">ABOUT US</a>
+					            <a href="#">PORTFOLIO</a>
+					            <a href="#">PRICING</a>
+					            <a href="#">TEAM</a>
+					            <a href="#">BLOG</a>
+					            <a href="#">CONTACT</a>
+					        </nav>
+					    </div>
+				
+					<!-- burger menu-END -->
+
+						<nav class="menu">
+							<ul class="menu_ul">
+								<li class="menu_item"><a href="#!" class="menu_active">HOME</a></li>
+								<li class="menu_item"><a href="#!">ABOUT US</a></li>
+								<li class="menu_item"><a href="#!">PORTFOLIO</a></li>
+								<li class="menu_item"><a href="#!">PRICING</a></li>
+								<li class="menu_item"><a href="#!">TEAM</a></li>
+								<li class="menu_item"><a href="#!">BLOG</a></li>
+								<li class="menu_item"><a href="#!">CONTACT</a></li>
+							</ul>
+						</nav> 
+					</div> <!-- ..//col -->
+				</div> <!-- ..//row -->
+				<div class="row slider_content">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<h2>Our Clients Are Our First Priority</h2>
+						<h1>WELCOME TO BINO</h1>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum <br> has been the industry's standard dummy text ever since the 1500s, when an unknown <br>printer took a galley of type and scrambled it to make a type specimen book.</p>
+						<div class="btn_header">
+							<a href="#!" class="btn_style">GET STARTED NOW</a>
+							<a href="#!" class="btn_style">LEARN MORE</a>
+						</div>
+					</div>
+				</div> <!-- ..//row -->
+			</div><!-- ..//header_content -->
+		</div><!-- ..//container -->
+
+		<!-- Header slider -->
+		<div class="prev_arrows1"></div>
+		<div class="next_arrows1"></div>
+		<div id="sliders" class="header__slider">
+			<div>
+				<img src="img/header_slide1.jpg" alt="">
+			</div>
+			<div>
+				<img src="img/header_slide1.jpg" alt="">
+			</div>
+			<div>
+				<img src="img/header_slide1.jpg" alt="">
+			</div>
+			<div>
+				<img src="img/header_slide1.jpg" alt="">
+			</div>
+		</div>
+		<!--..// Header slider -->
+
+	</header>
+
+	<!-- Screen 2 -->
+	<div class="screen2">
+		<div class="container-fluid p-0">
+			<div class="row m-0"> 
+			    <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+			        <div class="screen2_block ">
+			            <div class="row m-0">
+			                <div class="col-sm-6 p-0">
+			                    <div class="screen2_block_img">
+			                        <img src="img/screen2_image1.jpg" alt="">
+			                    </div>
+			                </div>
+			                <div class="col-sm-6 d-flex screen2_block_text">
+			                    <div class="justify-content-center align-items-center">
+			                        <p>art/t-shirt</p>
+			                        <h3>T-SHIRT DESIGN</h3>
+			                        <hr class="block_text_line">
+			                        <p class="pre_txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indu Stry's 
+			                            standard dummy text ever since the 1500s, an unknown printer took a galley of type 
+			                            a scrambled it to make a type specimen book.</p>
+
+			                        <a href="" class="read_more left_arrow1">Read More <i class="icon-double-angle-right"></i></a>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			    </div>
+
+			    <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+			        <div class="screen2_block ">
+			            <div class="row m-0">
+			                <div class="col-sm-6 p-0 order-md-6 order-sm-6 order-lg-0">
+			                    <div class="screen2_block_img">
+			                        <img src="img/screen2_image2.jpg" alt="">
+			                    </div>
+			                </div>
+			                <div class="col-sm-6 d-flex screen2_block_text">
+			                    <div class="justify-content-center align-items-center">
+			                        <p>art/t-shirt</p>
+			                        <h3>T-SHIRT DESIGN</h3>
+			                        <hr class="block_text_line">
+			                        <p class="pre_txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indu Stry's 
+			                            standard dummy text ever since the 1500s, an unknown printer took a galley of type 
+			                            a scrambled it to make a type specimen book.</p>
+
+			                        <a href="" class="read_more left_arrow2">Read More <i class="icon-double-angle-right"></i></a>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			    </div>
+			    <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+			        <div class="screen2_block ">
+			            <div class="row m-0">
+
+			                <div class="col-sm-6 order-md-0 order-sm-0 order-lg-6 p-0">
+			                    <div class="screen2_block_img">
+			                        <img src="img/screen2_image3.jpg" alt="">
+			                    </div>
+			                </div>
+
+			                <div class="col-sm-6 d-flex screen2_block_text">
+			                    <div class="justify-content-center align-items-center">
+			                        <p>art/t-shirt</p>
+			                        <h3>T-SHIRT DESIGN</h3>
+			                        <hr class="block_text_line">
+			                        <p class="pre_txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indu Stry's 
+			                            standard dummy text ever since the 1500s, an unknown printer took a galley of type 
+			                            a scrambled it to make a type specimen book.</p>
+
+			                        <a href="" class="read_more right_arrow1">Read More <i class="icon-double-angle-right"></i></a>
+			                    </div>
+			                </div>
+
+			            </div>
+			        </div>
+			    </div>
+
+			    <div class="col-lg-6 col-md-12 col-sm-12 p-0">
+			        <div class="screen2_block ">
+			            <div class="row m-0">
+			                <div class="col-sm-6 order-md-6 order-sm-6 order-lg-6 p-0">
+			                    <div class="screen2_block_img">
+			                        <img src="img/screen2_image4.jpg" alt="">
+			                    </div>
+			                </div>
+			                <div class="col-sm-6 d-flex screen2_block_text">
+			                    <div class="justify-content-center align-items-center">
+			                        <p>art/t-shirt</p>
+			                        <h3>T-SHIRT DESIGN</h3>
+			                        <hr class="block_text_line">
+			                        <p class="pre_txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indu Stry's standard dummy text ever since the 1500s, an unknown printer took a galley of type a scrambled it to make a type specimen book.</p>
+
+			                        <a href="" class="read_more right_arrow2">Read More <i class="icon-double-angle-right"></i></a>
+			                    </div>
+			                </div>
+
+			            </div>
+			        </div>
+			    </div>
+			</div>
+		</div>
+	</div> 
+	<!-- ..//Screen 2 -->
+
+
+
+		<div class="screen3_bg">
+			<div class="container">
+				<div class="row screen3 ">
+					<div class="col-12 text-center">
+						<h2>Let's Get Started Now. It's FREE!</h2>
+						<p>30 day free trial. Free plan allows up to 2 projects. Pay if you need more. Cancel anytime. No catches.</p>
+						<a href="javascript:void(0)" class="btn_style">start free trial</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-6 col-12 p-0">
+						<nav class="footer__social">
+							<ul>
+								<li><a href="javascript:void(0)" class="icon-facebook"></a></li>
+								<li><a href="javascript:void(0)" class="icon-twitter"></a></li>
+								<li><a href="javascript:void(0)" class="icon-rss"></a></li>
+								<li><a href="javascript:void(0)" class="icon-google-plus"></a></li>
+								<li><a href="javascript:void(0)" class="icon-linkedin"></a></li>
+								<li><a href="javascript:void(0)" class="icon-skype"></a></li>
+								<li><a href="javascript:void(0)" class="v_social fab fa-vimeo-v"></a></li>
+								<li><a href="javascript:void(0)" class="icon-tumblr"></a></li>
+							</ul>
+						</nav>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-6 col-12 p-0 ml-auto copuright">
+						<span>Kazi Erfan &copy; All Rights Reserved </span>
+					</div>
+				</div>
+			</div>
+		</footer>
+	
+</body>
+
+
+<script  src="https://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+
+<!-- Site Sripts -->
+<script src="js/functions.js"></script>
+
+</html>
---
+++ public_html/js/functions.js
@@ -0,0 +1,6 @@
+jQuery(function() {  
+	$('.header__slider').slick({
+		prevArrow:$('.prev_arrows1'),
+		nextArrow:$('.next_arrows1'),
+	});
+});
